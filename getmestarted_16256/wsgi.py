498b2343649d7178ca7e34a1aa2901ece9610e70
Crowdbotics <team@crowdbotics.com>
2020-04-25 18:44:53 +0000

    Update models

--- getmestarted_16256/wsgi.py
+++ getmestarted_16256/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'getmestarted_16256.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "getmestarted_16256.settings")
 
 application = get_wsgi_application()
